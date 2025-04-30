d2ad6ab43848e29a0654e02d0c3f0bf5240b48ca
MeetOswal <71141902+MeetOswal@users.noreply.github.com>
2025-04-30 11:25:18 -0400

    added images to vertical bar

--- src/components/VerticalCarousel.jsx
+++ src/components/VerticalCarousel.jsx
@@ -6,9 +6,8 @@ import img3 from "../assets/img3.png";
 import img4 from "../assets/img4.png";
 import img5 from "../assets/img5.png";
 import img6 from "../assets/img6.png";
-import img7 from "../assets/img7.png";
 
-const items = [img1, img2, img3, img4, img5, img6, img7];
+const items = [img1, img2, img3, img4, img5, img6];
 
 export default function VerticalCarousel() {
   return (
