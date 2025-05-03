e53ef2eae2664326bcd8ebf787d949269e4b6c9c
MeetOswal <71141902+MeetOswal@users.noreply.github.com>
2025-05-03 11:46:39 -0400

    added-image to vertical

--- src/components/VerticalCarousel.jsx
+++ src/components/VerticalCarousel.jsx
@@ -6,10 +6,11 @@ import img3 from "../assets/img3.png";
 import img4 from "../assets/img4.png";
 import img5 from "../assets/img5.png";
 import img6 from "../assets/img6.png";
+import img7 from "../assets/img7.png";
 
 
 export default function VerticalCarousel() {
-  const items = [img1, img2, img3, img4, img5, img6];
+  const items = [img1, img2, img3, img4, img5, img6, img7];
   return (
     <div className="vertical-carousel-container">
       <div className="vertical-carousel-track">
