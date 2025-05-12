069d4ecb4ed9f1a03437193ed57c6a345e2676ad
MeetOswal <71141902+MeetOswal@users.noreply.github.com>
2025-05-11 23:42:36 -0400

    sidebar resolution

--- apps/admin/src/components/SideBar.jsx
+++ apps/admin/src/components/SideBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./Sidebar.css";
 import { useNavigate } from "react-router-dom";
 import './SideBar.css';
 const Sidebar = ({ isMobile, isSidebarOpen, toggleSidebar }) => {
